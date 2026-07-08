913780ae99d72700aca12485c4cb4b38dfcee343
Karthika <karthikaindhumathi110@gmail.com>
2026-07-08 16:00:14 +0530

    Update CorsConfig.java

--- src/main/java/com/example/solarsync/security/CorsConfig.java
+++ src/main/java/com/example/solarsync/security/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             "http://localhost:5000",
             "http://localhost:5500",
             "http://127.0.0.1:5500",
-            "https://karthika1210selvam.github.io/solarprojectfrontend/"
+            "https://karthika1210selvam.github.io"
         ));
         config.setAllowedMethods(List.of(
           "GET",
